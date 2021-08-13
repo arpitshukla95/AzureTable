e00071c64648a5b119344ff1c782267711a4dfc3
Arpit Shukla <arpitshukla95@gmail.com>
2021-08-13 17:46:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+
+namespace AzureTable
+{
+    class Program
+    {
+        private static string connection_string = "CONNECTION_STRING";
+        private static string tableName = "customer";
+        static void Main(string[] args)
+        {
+            CloudStorageAccount _account = CloudStorageAccount.Parse(connection_string);
+            CloudTableClient _table_client = _account.CreateCloudTableClient();
+            CloudTable cloudTable =  _table_client.GetTableReference(tableName);
+            cloudTable.CreateIfNotExists();
+            Console.WriteLine("Table has been created");
+            Console.ReadKey();
+        }
+    }
+}
